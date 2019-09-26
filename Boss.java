3d90aae3b28b9db27c094186b72e549414716a02
hamzah <haldolaay@gmail.com>
2019-09-26 13:46:09 -0700

    working but i need to fix the input

--- Boss.java
+++ Boss.java
@@ -5,30 +5,31 @@
 	private Address addr;
 
 	
-	public Employee(String fname,String lname,Employee boss,String city, String state){
+	 Employee(String fname,String lname,Employee boss,String city, String state){
 		this.fname = fname;
 		this.lname = lname;
-		this.setBoss(boss);
-		//boss != null?this.setBoss(boss):;
+		//this.setBoss(boss);
+		if (boss != null){this.setBoss(boss);}
 		this.addr = new Address(city,state);
 	}
 
 	public String getName(){
 		return this.fname+" "+this.lname;
 	}			//returns the concatenated string fname and lname
-	public Address getAddress(){
-		return this.addr;
-	}			//returns Address object
+	public String  getAddress(){
+		return this.addr.getAddress();
+	}			//returns Address object\
+				//return this.addr.getAddress();
 	public void setBoss(Employee object){
-		if (object.boss != null){
-		//this.boss = new Employee(null,null,object.boss,null,null);
+		if (object.fname != null){
+		this.boss = new Employee(object.fname,null,null,null,null);
 		//this.boss = new Employee().fname = object.fname;
 		/*this.boss.fname = object.fname;
 		this.boss.lname = object.lname;
 		this.boss.boss = object.boss;
 		this.boss.addr = object.addr;
 		*/
-		this.boss= new Employee(object.fname,object.lname,object.boss,object.city,object.state);
+		//this.boss= new Employee(object.fname,object.lname,object.boss,object.addr);
 		};
 	}	//assigns the boss of the current employee. The boss is really JUST ANOTHER EMPLOYEE!!!!!!
 	public Employee getBoss(){return this.boss;}//returns the boss object which is just a reference to another employee
@@ -56,7 +57,7 @@ class Driver{
 		Employee dave = new Employee("dave","bobo",null,"sacramento","california");
 		Employee kevin = new Employee("kevin","dodo",dave,"london","england");
 		System.out.println(dave.getAddress());
-		
+		System.out.println(dave.toString());
 		
 		
 		

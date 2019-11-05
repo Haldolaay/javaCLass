7a06c35e7ec9bc416ee95634888d4a687e306957
hamzah <haldolaay@gmail.com>
2019-11-05 10:12:08 -0800

    trying out interfaces

---
+++ Interface.java
@@ -0,0 +1,56 @@
+interface Amount { 
+		void setPrice();
+ }
+
+abstract class Book implements Amount {
+	String title;
+	double price;
+	Book(String title){this.title = title;};
+	public String getTitle(){return this.title;}
+	public double getPrice(){return this.price;}
+	public abstract void setPrice();
+}
+
+final class Fiction extends Book{
+	String rating;
+	public void setPrice(){this.price = 24.99;};
+	Fiction(String title){
+		super(title);
+		setPrice();
+		this.rating = "good";
+	}
+	public String getRating(){return this.rating;}
+}
+
+class Adults extends NonFiction {
+	String level;	
+	Adults(String title){
+		super(title);
+		setPrice();
+	}
+	public void setPrice(){
+		
+	}
+}
+
+class NonFiction extends Book {  
+		NonFiction(String title){
+			super(title);
+			setPrice();
+		}
+	public void setPrice(){this.price = 37.99;}
+
+  }
+
+class Driver {
+	public static void main(String[]args){
+	 Book[] myBooks = new Book[4];
+			myBooks[0] = new Fiction("hoho");
+			myBooks[1] = new NonFiction("scary");
+			System.out.println(myBooks[1].getTitle());
+			System.out.println(myBooks[1].getPrice());
+			System.out.println((myBooks[0]).getTitle());
+			System.out.println(myBooks[0].getPrice());
+			System.out.println(((Fiction)myBooks[0]).getRating());
+	}
+}

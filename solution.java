dcaf6b9f2be9d818503370e0be44e07ac4e7feb2
Hamzah Aldolaay <41927016+Haldolaay@users.noreply.github.com>
2020-01-07 21:11:24 -0800

    Update solution.java

--- solution.java
+++ solution.java
@@ -1,6 +1,7 @@
 class Solution {
 	// this function will look into an array of numbers and 
 	// find the sum of two numbers adding up to the target argument
+	// I wanted to try out calling a static function without instantiating a class object
     public static  int[] twoSum(int[] nums, int target) {
    for (int i = 0 ; i < nums.length ; i++){
        for (int j = 0 ; j < nums.length ; j++){

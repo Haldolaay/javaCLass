7e67db9e9aa3e0295fa58bd30e3e7e1e5057f8c3
Hamzah Aldolaay <41927016+Haldolaay@users.noreply.github.com>
2020-11-05 00:23:50 -0800

    Update randomCode.java

--- randomCode.java
+++ randomCode.java
@@ -1,1 +1,10 @@
 
+public static void flipHorizontalAxis(int[][] matrix) {
+     int[] temp  ; int count = 0;
+     
+     for(int i = 0,k = matrix.length-1 ; i < k ; i++ , k--){
+       temp = matrix[i];
+       matrix[i] = matrix[k];
+       matrix[k] = temp;
+     }
+}
